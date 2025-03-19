8d06ed690936deee106d28617675fe7fefd5c88f
DTT26 <tungdtde180564@fpt.edu.vn>
2025-03-19 11:10:26 +0700

    ttt

--- QLTV/src/Service/CustomerService.java
+++ QLTV/src/Service/CustomerService.java
@@ -5,27 +5,17 @@ import Model.Account;
 import Model.Customer;
 import Model.CustomerBorrow;
 import Model.CustomerBuy;
-import View.view;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.Map;
-import ultils.CustomerValidation;
-import ultils.Validation;
+import java.util.HashSet;
 
 public class CustomerService implements Service<Customer> {
-    CustomerValidation cusVal = new CustomerValidation();
-    Validation val = new Validation();
     CustomerDAO cusDao = new CustomerDAO();
-    View.view view = new view();
-    private HashMap<Integer, Account> customerACC = new HashMap<>();
+    private HashSet<Account> customerACC = new HashSet<>();
 
     public Boolean CheckAccount(String userName, String Pass) {
-        customerACC = cusDao.getCustomerACC();
-        for (Map.Entry<Integer, Account> entry : customerACC.entrySet()) {
-            Integer key = entry.getKey();
-            Account value = entry.getValue();
-            if (value.getUsername().equals(userName) && value.getPass().equals(Pass)) {
+        for (Account account : customerACC) {
+            if (account.getUsername().equals(userName) && account.getPass().equals(Pass)) {
                 return true;
             }
         }
@@ -52,41 +42,10 @@ public class CustomerService implements Service<Customer> {
 
     @Override
     public void display(Customer entity) {
-        // No output needed
     }
-
-    public void update(int choice, String id, Object Value) {
-        switch (choice) {
-            case 1:
-                String newName = (String) Value;
-                cusDao.updateName(id, newName);
-                break;
-            case 2:
-                String newAddress = (String) Value;
-                cusDao.updateAddress(id, newAddress);
-                break;
-            case 3:
-                String newPhone = (String) Value;
-                cusDao.updatePhoneNumber(id, newPhone);
-                break;
-            case 4:
-                String newEmail = (String) Value;
-                cusDao.updateEmail(id, newEmail);
-                break;
-            case 5:
-                String NewBirthDate = (String) Value;
-                cusDao.updateBirthDate(id, NewBirthDate);
-                break;
-            case 6:
-                double totalPayment = (double) Value;
-                cusDao.updateTotalPayment(id, totalPayment);
-                break;
-            case 7:
-                int accountId = (int) Value;
-                cusDao.updateAccountId(id, accountId);
-                break;
-        }
-
+    
+    public void update(Customer customer) {
+        cusDao.update(customer);
     }
 
     @Override
@@ -94,4 +53,5 @@ public class CustomerService implements Service<Customer> {
         cusDao.insert(entity);
         return entity;
     }
+    
 }
