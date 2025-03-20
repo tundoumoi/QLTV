e3627386c2e763287615dddd22d72ff0e6b83ce2
binbinhl1 <tuyetvohl1@gmail.com>
2025-03-20 19:19:00 +0700

    billdao

--- QLTV/src/DAO/BillDAO.java
+++ QLTV/src/DAO/BillDAO.java
@@ -5,37 +5,122 @@
 package DAO;
 
 import Model.Bill;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.HashMap;
 
 /**
  *
- * @author Admin
+ * @author HP
  */
 public class BillDAO implements GenericDAO<Bill, HashMap<Integer,Bill>>{
 
     @Override
     public HashMap<Integer, Bill> getAll() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        HashMap<Integer, Bill> billMap = new HashMap<>();
+        String sql = "SELECT * FROM Bill";
+
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery()) {
+
+            while (rs.next()) {
+                int billCode = rs.getInt("BillCode");
+                String bookID = rs.getString("BookID");
+                String employeeID = rs.getString("EmployeeID");
+                LocalDate time = rs.getDate("Time").toLocalDate();
+                float unitPrice = rs.getFloat("UnitPrice");
+
+                Bill bill = new Bill(billCode, bookID, employeeID, time, unitPrice);
+                billMap.put(billCode, bill);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return billMap;
     }
 
     @Override
     public Bill getById(String id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        String sql = "SELECT * FROM Bill WHERE BillCode = ?";
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setInt(1, Integer.parseInt(id));
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                int billCode = rs.getInt("BillCode");
+                String bookID = rs.getString("BookID");
+                String employeeID = rs.getString("EmployeeID");
+                LocalDate time = rs.getDate("Time").toLocalDate();
+                float unitPrice = rs.getFloat("UnitPrice");
+
+                return new Bill(billCode, bookID, employeeID, time, unitPrice);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @Override
-    public void insert(Bill entity) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void insert(Bill bill) {
+        String sql = "INSERT INTO Bill (BillCode, BookID, EmployeeID, Time, UnitPrice) VALUES (?, ?, ?, ?, ?)";
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setInt(1, bill.getBillCode());
+            pstmt.setString(2, bill.getBookID());
+            pstmt.setString(3, bill.getEmployeeID());
+            pstmt.setDate(4, java.sql.Date.valueOf(bill.getTime()));
+            pstmt.setFloat(5, bill.getUnitPrice());
+
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public void update(Bill entity) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void update(Bill bill) {
+        String sql = "UPDATE Bill SET BookID = ?, EmployeeID = ?, Time = ?, UnitPrice = ? WHERE BillCode = ?";
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, bill.getBookID());
+            pstmt.setString(2, bill.getEmployeeID());
+            pstmt.setDate(3, java.sql.Date.valueOf(bill.getTime()));
+            pstmt.setFloat(4, bill.getUnitPrice());
+            pstmt.setInt(5, bill.getBillCode());
+
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void delete(String id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        String sql = "DELETE FROM Bill WHERE BillCode = ?";
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setInt(1, Integer.parseInt(id));
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
-    
 }
+
+    
+
